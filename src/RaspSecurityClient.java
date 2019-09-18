cf04c048f04e687351efc7cda0808862fe18b9d5
Ekinoxx <fadow0711@gmail.com>
2019-09-18 18:03:54 +0200

    HelloWorld!

--- src/RaspSecurityClient.java
+++ src/RaspSecurityClient.java
@@ -1,7 +1,21 @@
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.RaspiPin;
+
 public class RaspSecurityClient {
 
+	public static final GpioController GPIO = GpioFactory.getInstance();
+	
 	public static void main(String[] args) {
+		GpioPinDigitalOutput myLed = GPIO.provisionDigitalOutputPin(RaspiPin.GPIO_25);
 		
+		while(true) {
+			myLed.toggle();
+			try {
+				Thread.sleep(1000L);
+			} catch (InterruptedException e) {}
+		}
 	}
 
 }
