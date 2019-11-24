8de31236128fb24d42f8ba4d6b2eb51f8f7fc221
Ekinoxx <fadow0711@gmail.com>
2019-11-24 13:45:18 +0100

    Added password managment & some content

--- src/me/security/AppClient.java
+++ src/me/security/AppClient.java
@@ -1,13 +1,15 @@
 package me.security;
 
+import me.security.managers.DatabaseManager;
 import me.security.managers.NotificationManager;
 import me.security.managers.SecurityManager;
 
 public class AppClient {
 
 	public static void main(String[] args) {
+		DatabaseManager db = new DatabaseManager();
 		NotificationManager notif = new NotificationManager();
-		SecurityManager security = new SecurityManager(notif);
+		SecurityManager security = new SecurityManager(notif, db);
 		
 	}
 
--- src/me/security/managers/SecurityManager.java
+++ src/me/security/managers/SecurityManager.java
@@ -3,10 +3,12 @@ package me.security.managers;
 public class SecurityManager {
 	
 	private final NotificationManager notif;
+	private final DatabaseManager db;
 	private boolean enabled = true;
 	
-	public SecurityManager(NotificationManager notifications) {
+	public SecurityManager(NotificationManager notifications, DatabaseManager db) {
 		this.notif = notifications;
+		this.db = db;
 	}
 	
 }
