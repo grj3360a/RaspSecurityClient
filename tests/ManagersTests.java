eb70705355eaec9cd93b41c3714569d3f7657c08
Joseph <Joseph@LAPTOP-QODGLIPU>
2019-12-22 19:03:20 +0100

    Oopsie must remove it from managers test too

--- tests/ManagersTests.java
+++ tests/ManagersTests.java
@@ -5,8 +5,7 @@ import org.junit.runners.Suite.SuiteClasses;
 import managers.DatabaseTest;
 import managers.NotifManagerTest;
 import managers.RestAPITest;
-import managers.SecuTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ RestAPITest.class, DatabaseTest.class, SecuTest.class, NotifManagerTest.class })
+@SuiteClasses({ RestAPITest.class, DatabaseTest.class, NotifManagerTest.class })
 public class ManagersTests {}
